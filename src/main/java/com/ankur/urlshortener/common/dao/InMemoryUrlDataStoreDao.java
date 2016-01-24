a63dc95164120c9a7a59a301b10e2db5d07d138e
Ankur Agarwal <akahank@gmail.com>
2016-01-24 16:29:56 +0000

    Adding functionality to get stored urls with id

--- src/main/java/com/ankur/urlshortener/common/dao/InMemoryUrlDataStoreDao.java
+++ src/main/java/com/ankur/urlshortener/common/dao/InMemoryUrlDataStoreDao.java
@@ -2,35 +2,37 @@ package com.ankur.urlshortener.common.dao;
 
 import com.ankur.urlshortener.common.model.DataStoreId;
 import com.ankur.urlshortener.common.model.OriginalUrl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 @Repository
 class InMemoryUrlDataStoreDao implements UrlDataStoreDao {
 
-    private final Map<OriginalUrl, DataStoreId> dataStore;
+    private final InMemoryDataStore inMemoryDataStore;
     private int count;
 
-    InMemoryUrlDataStoreDao() {
-        dataStore = new HashMap<>();
+    @Autowired
+    InMemoryUrlDataStoreDao(final InMemoryDataStore inMemoryDataStore) {
+        this.inMemoryDataStore = inMemoryDataStore;
         count = 0;
     }
 
     @Override
     public DataStoreId storeOriginalUrl(OriginalUrl originalUrl) {
-        DataStoreId id = dataStore.get(originalUrl);
-        if (id == null) {
+        Optional<DataStoreId> optionalId = inMemoryDataStore.get(originalUrl);
+        if (!optionalId.isPresent()) {
             ++count;
-            id = DataStoreId.of(count);
-            dataStore.put(originalUrl, id);
+            DataStoreId id = DataStoreId.of(count);
+            inMemoryDataStore.store(originalUrl, id);
+            return id;
         }
-        return id;
+        return optionalId.get();
     }
 
     @Override
-    public OriginalUrl getOriginalUrl(DataStoreId dataStoreId) {
-        return null;
+    public Optional<OriginalUrl> getOriginalUrl(DataStoreId dataStoreId) {
+        return inMemoryDataStore.get(dataStoreId);
     }
 }
--- src/test/java/com/ankur/urlshortener/common/dao/InMemoryUrlDataStoreDaoTest.java
+++ src/test/java/com/ankur/urlshortener/common/dao/InMemoryUrlDataStoreDaoTest.java
@@ -5,8 +5,11 @@ import com.ankur.urlshortener.common.model.OriginalUrl;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class InMemoryUrlDataStoreDaoTest {
 
@@ -14,7 +17,7 @@ public class InMemoryUrlDataStoreDaoTest {
 
     @Before
     public void setup() {
-        objectUnderTest = new InMemoryUrlDataStoreDao();
+        objectUnderTest = new InMemoryUrlDataStoreDao(new InMemoryDataStore());
     }
 
     @Test
@@ -44,4 +47,48 @@ public class InMemoryUrlDataStoreDaoTest {
         assertEquals("2", response2.getValue());
     }
 
+    @Test
+    public void ensureDuplicateUrlsReturnTheSameId() {
+        // given
+        OriginalUrl originalUrl1 = OriginalUrl.of("http://www.google.com");
+        OriginalUrl originalUrl2 = OriginalUrl.of("http://www.google.com");
+
+        // when
+        DataStoreId response1 = objectUnderTest.storeOriginalUrl(originalUrl1);
+        DataStoreId response2 = objectUnderTest.storeOriginalUrl(originalUrl2);
+
+        // then
+        assertEquals("1", response1.getValue());
+        assertEquals("1", response2.getValue());
+    }
+
+    @Test
+    public void getOriginalUrlWhenIdPassedIn() {
+        // given
+        OriginalUrl originalUrl1 = OriginalUrl.of("http://www.google.com");
+        OriginalUrl originalUrl2 = OriginalUrl.of("http://www.google.com");
+        objectUnderTest.storeOriginalUrl(originalUrl1);
+        DataStoreId dataStoreId = DataStoreId.of(1);
+
+        // when
+        Optional<OriginalUrl> response = objectUnderTest.getOriginalUrl(dataStoreId);
+
+        // then
+        assertEquals(originalUrl1, response.get());
+    }
+
+    @Test
+    public void ensureEmptyResponseWhenIdDoesNotMatchAnything() {
+        // given
+        OriginalUrl originalUrl1 = OriginalUrl.of("http://www.google.com");
+        OriginalUrl originalUrl2 = OriginalUrl.of("http://www.google.com");
+        objectUnderTest.storeOriginalUrl(originalUrl1);
+        DataStoreId dataStoreId = DataStoreId.of(3);
+
+        // when
+        Optional<OriginalUrl> response = objectUnderTest.getOriginalUrl(dataStoreId);
+
+        // then
+        assertFalse(response.isPresent());
+    }
 }
