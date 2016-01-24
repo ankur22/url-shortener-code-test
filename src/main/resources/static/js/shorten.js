319bd86bea8e3a1a91d4a3ffab64de7c539f5b21
Ankur Agarwal <akahank@gmail.com>
2016-01-24 17:43:13 +0000

    Hooking in the front-end to th shorten url

--- src/main/resources/static/js/shorten.js
+++ src/main/resources/static/js/shorten.js
@@ -1,10 +1,17 @@
 function shortenUrl() {
-//    $.post("demo_test_post.asp",
-//    {
-//        name: "Donald Duck",
-//        city: "Duckburg"
-//    },
-//    function(data, status){
-//        alert("Data: " + data + "\nStatus: " + status);
-//    });
+    var urlVar = $('#original-url').val();
+    var requestBody = {url: urlVar};
+    $.ajax({
+        type: 'POST',
+        url: '/shorten',
+        data: JSON.stringify(requestBody),
+        success: function(data) {
+            $('#response').val(data.url)
+        },
+        error: function(xhr,status,error) {
+            $('#response').val("Please enter a valid url")
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
 }
